0c7f808c99818308a335315aec153e8305d35702
knguyen-academy <knguyen.academy@gmail.com>
2018-03-24 00:44:11 -0700

    Travel function
    Added prev,next,slideshow button

--- SchoolAppProject/Album_UC.Designer.cs
+++ SchoolAppProject/Album_UC.Designer.cs
@@ -29,27 +29,30 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Album_UC));
             this.Mid_panel = new System.Windows.Forms.Panel();
+            this.next_button = new System.Windows.Forms.Button();
+            this.previous_button = new System.Windows.Forms.Button();
             this.Bottom_panel = new System.Windows.Forms.Panel();
-            this.Next_button = new System.Windows.Forms.Button();
-            this.Previous_button = new System.Windows.Forms.Button();
             this.Slider_pictureBox = new System.Windows.Forms.PictureBox();
+            this.SlideShow_button = new System.Windows.Forms.Button();
+            this.Back_button = new System.Windows.Forms.Button();
             this.Album_label = new System.Windows.Forms.Label();
             this.Top_panel = new System.Windows.Forms.Panel();
-            this.Back_button = new System.Windows.Forms.Button();
             this.Left_Panel = new System.Windows.Forms.FlowLayoutPanel();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Photoleft_panel = new System.Windows.Forms.FlowLayoutPanel();
             this.Mid_panel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.Slider_pictureBox)).BeginInit();
             this.Top_panel.SuspendLayout();
-            this.Left_Panel.SuspendLayout();
             this.SuspendLayout();
             // 
             // Mid_panel
             // 
+            this.Mid_panel.Controls.Add(this.Photoleft_panel);
+            this.Mid_panel.Controls.Add(this.next_button);
+            this.Mid_panel.Controls.Add(this.previous_button);
             this.Mid_panel.Controls.Add(this.Bottom_panel);
-            this.Mid_panel.Controls.Add(this.Next_button);
-            this.Mid_panel.Controls.Add(this.Previous_button);
             this.Mid_panel.Controls.Add(this.Slider_pictureBox);
             this.Mid_panel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.Mid_panel.Font = new System.Drawing.Font("Century Gothic", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -57,6 +60,38 @@
             this.Mid_panel.Name = "Mid_panel";
             this.Mid_panel.Size = new System.Drawing.Size(1044, 467);
             this.Mid_panel.TabIndex = 13;
+            // 
+            // next_button
+            // 
+            this.next_button.FlatAppearance.BorderSize = 0;
+            this.next_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.next_button.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.next_button.ForeColor = System.Drawing.Color.White;
+            this.next_button.Image = ((System.Drawing.Image)(resources.GetObject("next_button.Image")));
+            this.next_button.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.next_button.Location = new System.Drawing.Point(963, 194);
+            this.next_button.Name = "next_button";
+            this.next_button.Size = new System.Drawing.Size(38, 45);
+            this.next_button.TabIndex = 5;
+            this.next_button.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.next_button.UseVisualStyleBackColor = true;
+            this.next_button.Click += new System.EventHandler(this.next_button_Click);
+            // 
+            // previous_button
+            // 
+            this.previous_button.FlatAppearance.BorderSize = 0;
+            this.previous_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.previous_button.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.previous_button.ForeColor = System.Drawing.Color.White;
+            this.previous_button.Image = ((System.Drawing.Image)(resources.GetObject("previous_button.Image")));
+            this.previous_button.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.previous_button.Location = new System.Drawing.Point(963, 143);
+            this.previous_button.Name = "previous_button";
+            this.previous_button.Size = new System.Drawing.Size(38, 45);
+            this.previous_button.TabIndex = 5;
+            this.previous_button.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.previous_button.UseVisualStyleBackColor = true;
+            this.previous_button.Click += new System.EventHandler(this.previous_button_Click);
             // 
             // Bottom_panel
             // 
@@ -66,52 +101,65 @@
             this.Bottom_panel.Size = new System.Drawing.Size(1044, 74);
             this.Bottom_panel.TabIndex = 2;
             // 
-            // Next_button
-            // 
-            this.Next_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Next_button.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Next_button.Location = new System.Drawing.Point(978, 154);
-            this.Next_button.Name = "Next_button";
-            this.Next_button.Size = new System.Drawing.Size(41, 30);
-            this.Next_button.TabIndex = 0;
-            this.Next_button.Text = "Next";
-            this.Next_button.UseVisualStyleBackColor = true;
-            this.Next_button.Click += new System.EventHandler(this.Back_button_Click);
-            // 
-            // Previous_button
-            // 
-            this.Previous_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Previous_button.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Previous_button.Location = new System.Drawing.Point(6, 154);
-            this.Previous_button.Name = "Previous_button";
-            this.Previous_button.Size = new System.Drawing.Size(25, 30);
-            this.Previous_button.TabIndex = 0;
-            this.Previous_button.Text = "Previous";
-            this.Previous_button.UseVisualStyleBackColor = true;
-            this.Previous_button.Click += new System.EventHandler(this.Back_button_Click);
-            // 
             // Slider_pictureBox
             // 
             this.Slider_pictureBox.Image = global::SchoolAppProject.Properties.Resources.Japan;
-            this.Slider_pictureBox.Location = new System.Drawing.Point(225, 6);
+            this.Slider_pictureBox.Location = new System.Drawing.Point(133, 6);
             this.Slider_pictureBox.Name = "Slider_pictureBox";
-            this.Slider_pictureBox.Size = new System.Drawing.Size(646, 381);
-            this.Slider_pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Slider_pictureBox.Size = new System.Drawing.Size(824, 381);
+            this.Slider_pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.Slider_pictureBox.TabIndex = 1;
             this.Slider_pictureBox.TabStop = false;
+            this.Slider_pictureBox.Click += new System.EventHandler(this.Slider_pictureBox_Click);
+            // 
+            // SlideShow_button
+            // 
+            this.SlideShow_button.FlatAppearance.BorderSize = 0;
+            this.SlideShow_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SlideShow_button.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SlideShow_button.ForeColor = System.Drawing.Color.White;
+            this.SlideShow_button.Image = ((System.Drawing.Image)(resources.GetObject("SlideShow_button.Image")));
+            this.SlideShow_button.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.SlideShow_button.Location = new System.Drawing.Point(341, 6);
+            this.SlideShow_button.Name = "SlideShow_button";
+            this.SlideShow_button.Size = new System.Drawing.Size(35, 45);
+            this.SlideShow_button.TabIndex = 5;
+            this.SlideShow_button.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.SlideShow_button.UseVisualStyleBackColor = true;
+            this.SlideShow_button.Click += new System.EventHandler(this.SlideShow_button_Click);
+            // 
+            // Back_button
+            // 
+            this.Back_button.FlatAppearance.BorderSize = 0;
+            this.Back_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Back_button.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Back_button.ForeColor = System.Drawing.Color.White;
+            this.Back_button.Image = ((System.Drawing.Image)(resources.GetObject("Back_button.Image")));
+            this.Back_button.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Back_button.Location = new System.Drawing.Point(991, 5);
+            this.Back_button.Name = "Back_button";
+            this.Back_button.Size = new System.Drawing.Size(38, 45);
+            this.Back_button.TabIndex = 4;
+            this.Back_button.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.Back_button.UseVisualStyleBackColor = true;
+            this.Back_button.Click += new System.EventHandler(this.Back_button_Click);
             // 
             // Album_label
             // 
             this.Album_label.AutoSize = true;
             this.Album_label.Font = new System.Drawing.Font("Century Gothic", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Album_label.Location = new System.Drawing.Point(-4, 3);
+            this.Album_label.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Album_label.Location = new System.Drawing.Point(382, 10);
             this.Album_label.Name = "Album_label";
             this.Album_label.Size = new System.Drawing.Size(269, 36);
             this.Album_label.TabIndex = 0;
             this.Album_label.Text = "AlbumUserControl";
+            this.Album_label.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // Top_panel
             // 
+            this.Top_panel.Controls.Add(this.SlideShow_button);
+            this.Top_panel.Controls.Add(this.Back_button);
             this.Top_panel.Controls.Add(this.Album_label);
             this.Top_panel.Dock = System.Windows.Forms.DockStyle.Top;
             this.Top_panel.Font = new System.Drawing.Font("Century Gothic", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -120,21 +168,8 @@
             this.Top_panel.Size = new System.Drawing.Size(1044, 57);
             this.Top_panel.TabIndex = 12;
             // 
-            // Back_button
-            // 
-            this.Back_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Back_button.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Back_button.Location = new System.Drawing.Point(3, 3);
-            this.Back_button.Name = "Back_button";
-            this.Back_button.Size = new System.Drawing.Size(53, 30);
-            this.Back_button.TabIndex = 0;
-            this.Back_button.Text = "Back";
-            this.Back_button.UseVisualStyleBackColor = true;
-            this.Back_button.Click += new System.EventHandler(this.Back_button_Click);
-            // 
             // Left_Panel
             // 
-            this.Left_Panel.Controls.Add(this.Back_button);
             this.Left_Panel.Dock = System.Windows.Forms.DockStyle.Left;
             this.Left_Panel.Font = new System.Drawing.Font("Century Gothic", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Left_Panel.Location = new System.Drawing.Point(0, 0);
@@ -146,6 +181,14 @@
             // 
             this.timer1.Interval = 2000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Photoleft_panel
+            // 
+            this.Photoleft_panel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.Photoleft_panel.Location = new System.Drawing.Point(7, 7);
+            this.Photoleft_panel.Name = "Photoleft_panel";
+            this.Photoleft_panel.Size = new System.Drawing.Size(120, 380);
+            this.Photoleft_panel.TabIndex = 6;
             // 
             // Album_UC
             // 
@@ -160,7 +203,6 @@
             ((System.ComponentModel.ISupportInitialize)(this.Slider_pictureBox)).EndInit();
             this.Top_panel.ResumeLayout(false);
             this.Top_panel.PerformLayout();
-            this.Left_Panel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -168,15 +210,16 @@
         #endregion
 
         private System.Windows.Forms.Panel Mid_panel;
-        private System.Windows.Forms.Button Next_button;
-        private System.Windows.Forms.Button Previous_button;
         private System.Windows.Forms.PictureBox Slider_pictureBox;
         private System.Windows.Forms.Label Album_label;
         private System.Windows.Forms.Panel Top_panel;
         private System.Windows.Forms.Panel Bottom_panel;
-        private System.Windows.Forms.Button Back_button;
         private System.Windows.Forms.FlowLayoutPanel Left_Panel;
         private System.Windows.Forms.Timer timer1;
-
+        private System.Windows.Forms.Button Back_button;
+        private System.Windows.Forms.Button next_button;
+        private System.Windows.Forms.Button previous_button;
+        private System.Windows.Forms.Button SlideShow_button;
+        private System.Windows.Forms.FlowLayoutPanel Photoleft_panel;
     }
 }
--- SchoolAppProject/Album_UC.cs
+++ SchoolAppProject/Album_UC.cs
@@ -12,8 +12,8 @@ namespace SchoolAppProject
 {
     public partial class Album_UC : UserControl
     {
-
-
+        private int  holdindex;
+        private int  photo_index = 1;
         private static Album_UC _instance;
 
         public static Album_UC Instance
@@ -35,36 +35,114 @@ namespace SchoolAppProject
         public Album_UC()
         {
             InitializeComponent();
-            //PicturesLoad();
-            //timer1.Tick += new EventHandler(timer1_Tick);
-            timer1.Interval = 700;             // Timer will tick evert 10 seconds
-            //timer1.Enabled = true;                       // Enable the timer
-            timer1.Start();                              // Start the timer
+           // ListAlbumPhotos();
+
         }
-        private int i=1;
+
+        private void ListAlbumPhotos()
+        {
+            for (int i = 1; i < 5; i++)
+            {
+                PictureBox p1 = new PictureBox();
+                p1.ImageLocation = string.Format(@"JapanPhotos\{0}.jpg", i);
+                p1.SizeMode = PictureBoxSizeMode.Zoom;
+                Photoleft_panel.Controls.Add(p1);
+                p1.Click += P1_Click;
+                holdindex = i;
+                //p1.Margin = new Padding(0);
+
+            }
+
+
+      
+        }
+
+        private void P1_Click(object sender, EventArgs e)
+        {
+
+            //MessageBox.Show("hele");
+            Slider_pictureBox.ImageLocation = string.Format(@"JapanPhotos\{0}.jpg", holdindex);
+        }
+
+        private void SelectAlbum()
+        {
+            if (Album_label.Text == "JAPAN ALBUM")
+            {
+                Slider_pictureBox.ImageLocation = string.Format(@"JapanPhotos\{0}.jpg", photo_index);
+            }
+
+            if (Album_label.Text == "CANADA ALBUM")
+            {
+                Slider_pictureBox.ImageLocation = string.Format(@"CanadaPhotos\{0}.jpg", photo_index);
+            }
+
+            if (Album_label.Text == "VIETNAM ALBUM")
+            {
+                Slider_pictureBox.ImageLocation = string.Format(@"VietnamPhotos\{0}.jpg", photo_index);
+            }
+
+            if (Album_label.Text == "NEWYORK ALBUM")
+            {
+                Slider_pictureBox.ImageLocation = string.Format(@"NewYorkPhotos\{0}.jpg", photo_index);
+            }
+
+            if (Album_label.Text == "LA ALBUM")
+            {
+                Slider_pictureBox.ImageLocation = string.Format(@"LAPhotos\{0}.jpg", photo_index);
+            }
+        }
+
+
         public void PicturesLoad()
         {
-            //Slider_pictureBox.ImageLocation = string.Format(@"JapanPhotos\{0}.jpg", 1);
-            //MessageBox.Show(i.ToString());
 
-            if (i == 4)
-              i = 1;
+            if (photo_index == 4)
+                photo_index = 1;
+            photo_index++;
 
-            Slider_pictureBox.ImageLocation = string.Format(@"JapanPhotos\{0}.jpg", i);
-            i++;
-
+            SelectAlbum();
 
         }
-        
-      
-        private void Back_button_Click(object sender, EventArgs e)
+
+        private void SlideShow_button_Click(object sender, EventArgs e)
         {
-            SendToBack();
+            timer1.Interval = 500;             // Timer will tick evert 0.5 seconds
+            //timer1.Enabled = true;             // Enable the timer
+            timer1.Start();                     //Fire up timer1_Tick function()
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             PicturesLoad();
+        }
+
+        private void Back_button_Click(object sender, EventArgs e)
+        {
+            SendToBack();
+        }
+
+        private void previous_button_Click(object sender, EventArgs e)
+        {
+            
+            if (photo_index == 1)
+                photo_index = 4;
+            photo_index--;
+            SelectAlbum();
+           
+        }
+
+        private void next_button_Click(object sender, EventArgs e)
+        {
+            if (photo_index == 4)
+                photo_index = 1;
+            photo_index++;
+            SelectAlbum();
+         
+        }
+
+        private void Slider_pictureBox_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
         }
     }
 }
