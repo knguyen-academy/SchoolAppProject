5d56dc94c5d363253d36aa91ae8aef4e0ffae6e8
knguyen-academy <knguyen.academy@gmail.com>
2018-03-25 00:18:37 -0700

    Revert - 7c4761
    Revert back to 7c4761 commit

--- SchoolAppProject/Album_UC.cs
+++ SchoolAppProject/Album_UC.cs
@@ -7,13 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace SchoolAppProject
 {
     public partial class Album_UC : UserControl
     {
-        private int  holdindex;
+       
         private int  photo_index = 1;
+        private string albumPath;
         private static Album_UC _instance;
 
         public static Album_UC Instance
@@ -35,11 +37,9 @@ namespace SchoolAppProject
         public Album_UC()
         {
             InitializeComponent();
-           // ListAlbumPhotos();
 
         }
 
-<<<<<<< HEAD
         private void SelectAlbumPath()
         {
             if (Album_label.Text == "JAPAN ALBUM")
@@ -57,103 +57,45 @@ namespace SchoolAppProject
             if (Album_label.Text == "LA ALBUM")
                 albumPath = "../../Images/LA";
         }
-        private string temp;
+
         private void DisplayAllPhotos()
         {
             string[] files = Directory.GetFiles(albumPath);
-
-            //foreach (string filename in files)
-            //{
-
-            //    PictureBox p1 = new PictureBox();
-            //    p1.Size = new Size(120, 90);
-            //    p1.Image = Image.FromFile(filename);
-            //    p1.SizeMode = PictureBoxSizeMode.StretchImage;
-            //    Photoleft_panel.Controls.Add(p1);
-            //    p1.Click += P1_Click;
-            //}
-            //int i = 0;
-
             foreach (string filename in files)
             {
-                for (int i = 0; i < 5; i++)
-                {
-                    temp = filename;
-
-                    PictureBox[] p = new PictureBox[5];
-                    p[i] = new PictureBox();
-                    p[i].Size = new Size(120, 90);
-                    p[i].Image = Image.FromFile(filename);
-
-                    p[i].SizeMode = PictureBoxSizeMode.StretchImage;
-                    Photoleft_panel.Controls.Add(p[i]);
-                    p[i].Click += P1_Click;
-
-                }
-            }
-
-        }
-
-        private void P1_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show(temp);
-            //SelectAlbum();
-        }
-    
-        private void ListAlbumPhotos()
-=======
-        private void ListAlbumPhotos()
-        {
-            for (int i = 1; i < 5; i++)
-            {
                 PictureBox p1 = new PictureBox();
-                p1.ImageLocation = string.Format(@"JapanPhotos\{0}.jpg", i);
-                p1.SizeMode = PictureBoxSizeMode.Zoom;
+                p1.Image = Image.FromFile(filename);
+                p1.SizeMode = PictureBoxSizeMode.StretchImage;
                 Photoleft_panel.Controls.Add(p1);
-                p1.Click += P1_Click;
-                holdindex = i;
-                //p1.Margin = new Padding(0);
-
             }
-
-
-      
         }
 
         private void P1_Click(object sender, EventArgs e)
->>>>>>> parent of 7c47611... Travel button
+        { 
+}
+        private void ListAlbumPhotos()
         {
-
-            //MessageBox.Show("hele");
-            Slider_pictureBox.ImageLocation = string.Format(@"JapanPhotos\{0}.jpg", holdindex);
+            //string[] files = Directory.GetFiles(@"Images\Japan");
+            SelectAlbumPath();
+            DisplayAllPhotos();
         }
 
         private void SelectAlbum()
         {
             if (Album_label.Text == "JAPAN ALBUM")
-            {
                 Slider_pictureBox.ImageLocation = string.Format(@"JapanPhotos\{0}.jpg", photo_index);
-            }
 
             if (Album_label.Text == "CANADA ALBUM")
-            {
                 Slider_pictureBox.ImageLocation = string.Format(@"CanadaPhotos\{0}.jpg", photo_index);
-            }
-
+            
             if (Album_label.Text == "VIETNAM ALBUM")
-            {
                 Slider_pictureBox.ImageLocation = string.Format(@"VietnamPhotos\{0}.jpg", photo_index);
-            }
 
             if (Album_label.Text == "NEWYORK ALBUM")
-            {
                 Slider_pictureBox.ImageLocation = string.Format(@"NewYorkPhotos\{0}.jpg", photo_index);
-            }
-
+            
             if (Album_label.Text == "LA ALBUM")
-            {
                 Slider_pictureBox.ImageLocation = string.Format(@"LAPhotos\{0}.jpg", photo_index);
-            }
         }
 
 
@@ -189,7 +131,7 @@ namespace SchoolAppProject
         {
             
             if (photo_index == 1)
-                photo_index = 4;
+                photo_index = 5;
             photo_index--;
             SelectAlbum();
            
@@ -198,7 +140,7 @@ namespace SchoolAppProject
         private void next_button_Click(object sender, EventArgs e)
         {
             if (photo_index == 4)
-                photo_index = 1;
+                photo_index = 0;
             photo_index++;
             SelectAlbum();
          
@@ -207,6 +149,14 @@ namespace SchoolAppProject
         private void Slider_pictureBox_Click(object sender, EventArgs e)
         {
             timer1.Stop();
+        }
+
+        private void showall_button_Click(object sender, EventArgs e)
+        {
+            if (Photoleft_panel.Controls.Count == 0)
+                ListAlbumPhotos();
+
+                
         }
     }
 }
